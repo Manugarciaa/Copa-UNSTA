3b42d39dea438213125a2e1bf489adcc9fdaa1da
Manugarciaa <104218614+Manugarciaa@users.noreply.github.com>
2024-04-09 12:32:57 -0300

    Actualizacion Home y Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,56 +1,61 @@
 import { useState } from "react";
 
-import { logo, close, menu } from "../assets"
+import { logo, close, menu } from "../assets";
 
-export const navLinks = [   {     id: "inicio",     title: "Inicio",   },   {     id: "torneo",     title: "Torneo",   },   {     id: "semana-de-la-ingenieria",     title: "Semana de la Ingeniería",   }, ];
+export const navLinks = [
+  { id: "inicio", title: "Inicio" },
+  { id: "torneo", title: "Torneo" },
+  { id: "semana-de-la-ingenieria", title: "Semana de la Ingeniería" },
+];
 
 const Header = () => {
-  const [toggle, settoggle] = useState(false)
+  const [active, setActive] = useState("Home");
+  const [toggle, setToggle] = useState(false);
   return (
-    <nav className='w-full flex py-6 justify-between items-center Header'>
-      <img src={logo} alt='UNSTA' className='w-[124] h-[32px]'/>
-    
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[40px]" />
+
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
-          <li 
+          <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-dimWhite"
+              } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}
           >
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
+            <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
         ))}
       </ul>
 
       <div className="sm:hidden flex flex-1 justify-end items-center">
-          <img 
+        <img
           src={toggle ? close : menu}
           alt="menu"
           className="w-[28px] h-[28px] object-contain"
-          onClick={() => settoggle((prev) => !prev)}
-          />
+          onClick={() => setToggle(!toggle)}
+        />
 
-          <div 
-            className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-lg sidebar`}>
-              <ul className="list-none flex-col justify-end items-center flex-1">
-              {navLinks.map((nav, index) => (
-                <li 
-                  key={nav.id}
-                  className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}
-                >
-                  <a href={`#${nav.id}`}>
-                    {nav.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-
+        <div
+          className={`${!toggle ? "hidden" : "flex"
+            } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-dimWhite"
+                  } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,79 @@
+import { logo_torneo, logo } from "../assets";
 import styles from "../style";
-import { logo_torneo } from "../assets";
 
 const Home = () => (
-  <section
-    id="home"
-    className={" flex md:flex-row flex-col ${styles.paddingY}"}
-  >
-    <div
-      className={
-        "flex md:flex-row flex-col ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6"
-      }
-    >
-      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradiant rounded-[10px] mb-2"></div>
-      <div className="flex flex-row justify-between items center w-full">
-        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-          Disfruta uniéndote del torneo <br className="sm:block hidden" />{" "}
-          <span className="text-gradient">UNSTA</span>{" "}
-          <p className="text-lg text-gray-400">
-            ¡Experimenta la emoción del deporte en nuestra universidad!
-          </p>
-        </h1>
+  <>
+    {/* Sección "Inicio" */}
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold text-[42px] text-white leading-[45px] xl:text-[65px] xl:leading-[75px]">
+            Bienvenidos al evento más emocionante de <span className="text-gradient">Ingeniería</span>
+          </h1>
+        </div>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          ¡No te pierdas las charlas y el torneo de futbol de la Semana de la Ingeniería!
+          Mantente informado y participa en esta emocionante semana llena de aprendizaje y competencia
+        </p>
       </div>
-    </div>
-    <div>
-      <img
-        src={logo_torneo}
-        alt="billing"
-        className="w-[80%] h-[90%] relative z-[5]"
-      />
-      <div className="absolute z-[0] w-[20%] h-[20%] top-0 left-[58%] orange__gradient" />
-    </div>
-  </section>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img
+          src={logo_torneo}
+          alt="Home image"
+          className="w-[80%] h-[100%] relative z-[5]"
+        />
+
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 white__gradient" />
+        <div className="absolute z-[1] w-[70%] h-[80%] rounded-full orange__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 gray__gradient" />
+        {/* gradient end */}
+      </div>
+    </section>
+
+    {/* Sección "Torneo" */}
+    <section id="Torneo" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 flex ${styles.flexStart} md:my-0 my-10 relative`}>
+        <img
+          src={logo_torneo}
+          alt="Home image"
+          className="w-[80%] h-[100%] relative z-[5]"
+        />
+      </div>
+
+      {/* Ver como hacer para que el contenedor de la imagen pase abajo en el celular */}
+
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold text-[42px] text-white leading-[45px] xl:text-[65px] xl:leading-[75px]">
+            ¡No dejes pasar la oportunidad! {" "}
+            <span className="text-gradient"> Inscríbete ahora.</span>
+          </h1>
+        </div>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          ¡No te pierdas las charlas y el torneo de futbol de la Semana de la Ingeniería!
+          Mantente informado y participa en esta emocionante semana llena de aprendizaje y competencia
+        </p>
+        {/* Botón Inscribirse */}
+        <button className="my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          ¡Inscríbete ahora!
+        </button>
+      </div>
+    </section>
+
+    <div className="my-6 border-b border-gray-300"></div>
+
+    {/* Sección "Semana de la Ingeniería" */}
+    <section id="semana-de-la-ingenieria" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div></div>
+    </section>
+  </>
 );
 
 export default Home;
+
+
+
+
+
